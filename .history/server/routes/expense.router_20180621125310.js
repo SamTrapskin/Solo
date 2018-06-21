94d805e7eb37eeb866657feccbbe874eaa408ac8
Sam Trapskin <samtrapskin@gmail.com>
2018-06-21 18:06:18 -0500

    added firebase

--- .history/server/routes/expense.router_20180621125310.js
+++ .history/server/routes/expense.router_20180621125310.js
@@ -22,26 +22,24 @@ router.get('/', (req, res, next) => {
 
 		//POST ROUTE TO SERVER FOR EXPENSE TABLE//
 
-
-		router.post('/', (req, res, next) => {
-			console.log('POST Route');
-			console.log(req.body);
-			if (req.isAuthenticated()) {
-				const queryText = `INSERT INTO items ("item_description", "purchase_date", "item_price", "item_link") VALUES ($1, $2, $3, $4);`;
-				pool
-					.query(queryText, [ req.body.item_description, req.body.purchase_date, req.body.item_price, req.body.item_link])
-					.then((result) => {
-						res.sendStatus(201);
-					})
-					.catch((err) => {
-						console.log(err);
-						res.sendStatus(500);
-					});
-			} else {
-				res.sendStatus(403);
-			}
-		});
-
+router.post('/', (req, res, next) => {
+	console.log('POST Route');
+	console.log(req.body);
+	if (req.isAuthenticated()) {
+		const queryText = `INSERT INTO employment_database ("company_name", "market", "application_status", "application_date", "response_received", "phone_interview_date", "notes") VALUES ($1, $2, $3, $4, $5);`;
+		pool
+			.query(queryText, [ req.body.company_name, req.body.market, req.body.application_status, req.body.application_date, req.body.response_received, req.body.phone_interview_date, req.body.notes])
+			.then((result) => {
+				res.sendStatus(201);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.sendStatus(500);
+			});
+	} else {
+		res.sendStatus(403);
+	}
+});
 		//END POST ROUTE
 
 
